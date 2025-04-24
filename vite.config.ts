9b9682027c13021f1330a8318b8f488bcb7dd7ca
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2025-04-24 15:21:11 +0900

    feat: added tauri

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
+   clearScreen: false,
+   server: {
+      port: 5173,
+      strictPort: true,
+      host: host || false,
+      hmr: host
+         ? {
+              protocol: 'ws',
+              host,
+              port: 1421,
+           }
+         : undefined,
+
+      watch: {
+         ignored: ['**/src-tauri/**'],
+      },
+   },
+   envPrefix: ['VITE_', 'TAURI_ENV_*'],
+   build: {
+      target: process.env.TAURI_ENV_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+      minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+      sourcemap: !!process.env.TAURI_ENV_DEBUG,
+   },
    plugins: [react()],
 });
