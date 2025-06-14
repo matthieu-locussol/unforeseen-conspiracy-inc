f5d8db7db7be47516bbc98430760c1059a2476b1
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2025-06-14 14:22:26 +0900

    feat: added toaster

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from 'sonner';
 
 import App from './App.tsx';
 import { StoreProvider } from './store/index.tsx';
@@ -10,6 +11,14 @@ import './styles/globals.css';
 createRoot(document.getElementById('root')!).render(
    <StrictMode>
       <StoreProvider>
+         <Toaster
+            closeButton
+            richColors
+            duration={3000}
+            position="bottom-right"
+            theme="dark"
+            visibleToasts={1}
+         />
          <App />
       </StoreProvider>
    </StrictMode>,
