ea21734f368434cbf46be7a8e678392fff9ee147
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2025-06-16 14:33:37 +0900

    style: layout is larger

--- src/components/core/Layout.tsx
+++ src/components/core/Layout.tsx
@@ -4,7 +4,7 @@ interface LayoutProps {
 
 export const Layout = ({ children }: LayoutProps) => {
    return (
-      <div className="flex flex-col justify-center relative z-10 mx-auto min-h-screen max-w-4xl px-4 py-8">
+      <div className="flex flex-col justify-center relative z-10 mx-auto min-h-screen max-w-5xl px-4 py-8">
          {children}
       </div>
    );
