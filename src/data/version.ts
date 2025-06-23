f86ed5b74a557466267b4a9cca45d1d212837a76
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2025-06-23 20:19:32 +0900

    chore(release): bump version to 0.0.13

--- src/data/version.ts
+++ src/data/version.ts
@@ -1,4 +1,4 @@
 // This file is auto-generated. Do not edit manually.
-export const VERSION_COMMIT = '1e638f4';
+export const VERSION_COMMIT = '9184dd2';
 
 export const VERSION_DATE = '2025-06-23';
