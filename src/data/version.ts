d5fe6fefdad68af88d9eb1e866d52f2cf989af70
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2025-06-23 14:52:24 +0900

    chore: bumped version

--- src/data/version.ts
+++ src/data/version.ts
@@ -1,4 +1,4 @@
 // This file is auto-generated. Do not edit manually.
-export const VERSION_COMMIT = 'ca0683f';
+export const VERSION_COMMIT = '877cd79';
 
 export const VERSION_DATE = '2025-06-23';
