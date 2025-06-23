64430a019d392c1cd923a9e4d81e2373f9ba5810
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2025-06-23 15:06:22 +0900

    chore(release): update version to 0.0.4 and reflect new commit '8e674ad'

--- src/data/version.ts
+++ src/data/version.ts
@@ -1,4 +1,4 @@
 // This file is auto-generated. Do not edit manually.
-export const VERSION_COMMIT = 'd5af1ba';
+export const VERSION_COMMIT = '8e674ad';
 
 export const VERSION_DATE = '2025-06-23';
