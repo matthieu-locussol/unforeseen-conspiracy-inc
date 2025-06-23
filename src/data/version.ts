e1f8cb5e571cc109ad9c33f79cb993db5a862dc0
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2025-06-23 15:53:11 +0900

    chore(release): update version to 0.0.9

--- src/data/version.ts
+++ src/data/version.ts
@@ -1,4 +1,4 @@
 // This file is auto-generated. Do not edit manually.
-export const VERSION_COMMIT = '540a0af';
+export const VERSION_COMMIT = '50ec839';
 
 export const VERSION_DATE = '2025-06-23';
