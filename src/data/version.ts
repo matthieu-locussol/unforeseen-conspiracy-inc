f0416c68966f73a820a8fe2c74df1fc0d50cb084
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2025-06-23 14:50:07 +0900

    chore: add version bumping script and update versioning in package.json and version.ts for improved version management

--- src/data/version.ts
+++ src/data/version.ts
@@ -1,4 +1,4 @@
 // This file is auto-generated. Do not edit manually.
-export const VERSION_COMMIT = 'b1820e3';
+export const VERSION_COMMIT = '97426be';
 
-export const VERSION_DATE = '2025-06-18';
+export const VERSION_DATE = '2025-06-23';
---
+++ src/scripts/bumpversion.ts
@@ -0,0 +1,31 @@
+import { readFileSync, writeFileSync } from 'fs';
+import { resolve } from 'path';
+
+const getVersion = () => {
+   const filePath = resolve(__dirname, '../../package.json');
+   const fileBlob = readFileSync(filePath, 'utf8');
+   const packageJson = JSON.parse(fileBlob);
+   const { version } = packageJson;
+
+   return version;
+};
+
+const bumpVersion = () => {
+   const version = getVersion();
+
+   const cargoFilePath = resolve(__dirname, '../../src-tauri/Cargo.toml');
+   const cargoFileBlob = readFileSync(cargoFilePath, 'utf8');
+   const newCargoFile = cargoFileBlob.replace(/version = .*/, `version = "${version}"`);
+
+   writeFileSync(cargoFilePath, newCargoFile);
+
+   ['../../src-tauri/tauri.conf.json'].forEach((path) => {
+      const clientFilePath = resolve(__dirname, path);
+      const clientFileBlob = readFileSync(clientFilePath, 'utf8');
+      const newClientFile = clientFileBlob.replace(/"version": .*/, `"version": "${version}",`);
+
+      writeFileSync(clientFilePath, newClientFile);
+   });
+};
+
+bumpVersion();
